418b4c2c7b923e4079d3e1b1e3442c451096e8bc
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-11-14 11:39:26 -0800

    Update words_in_both.py

--- words_in_both.py
+++ words_in_both.py
@@ -6,17 +6,18 @@
 Function below takes two sentences and finds words that are in both
 
 '''
-
 def words_in_both(sentence1, sentence2):
     newsentence1=sentence1.lower()
     newsentence2=sentence2.lower()
     string_array1=newsentence1.split()
     string_array2=newsentence2.split()
-    words=[]
+    words=set()
     for i in range(0,len(string_array1)-1):
             if string_array1[i] in string_array2:
-                words.append(string_array1[i])
+                words.add(string_array1[i])
     return words
+
+
 
 
 
