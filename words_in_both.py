c8871ba1121d36962d5e1855b0092f4f180a4873
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-11-14 11:28:39 -0800

    Update words_in_both.py

--- words_in_both.py
+++ words_in_both.py
@@ -10,21 +10,15 @@ Function below takes two sentences and finds words that are in both
 def words_in_both(sentence1, sentence2):
     newsentence1=sentence1.lower()
     newsentence2=sentence2.lower()
-    string_array1=newsentence1.split(" ")
-    string_array2=newsentence2.split(" ")
-    par_list=[]
-    par_list.append(sentence1)
+    string_array1=newsentence1.split()
+    string_array2=newsentence2.split()
+    words=[]
     for i in range(0,len(string_array1)-1):
-            if string_array1[i] not in string_array2:
-                del string_array1[i]
-    if len(string_array1) < len(sentence1.split(" ")):
-       return string_array1
-    elif len(string_array1) == 1 and string_array1[0] in string_array2:
-        return string_array1
-    else:
-        print("no words in common")
-#print(words_in_both("in", "from if to in"))
+            if string_array1[i] in string_array2:
+                words.append(string_array1[i])
+    return words
 
+print(words_in_both("hello my honey", "hello world"))
 
 
 
